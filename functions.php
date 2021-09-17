577777660c5c1e61123f189bfed826f2cf71cf2c
dufva <sussiedufva@hotmail.com>
2021-09-17 13:55:05 +0200

    post-thumbnail added to functions

--- functions.php
+++ functions.php
@@ -28,6 +28,7 @@ function load_js() {
 
   function add_themes_features() {
       add_theme_support('woocommerce');
+      add_theme_support('post-thumbnail');
   }
   add_action('after_setup_theme', 'add_themes_features');
 
