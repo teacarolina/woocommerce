255688c28ed55ae432f12097ae9c771c07c07a77
dufva <sussiedufva@hotmail.com>
2021-09-21 10:55:34 +0200

    category pics displayed on front page and blue color on header changed to beige

--- front-page.php
+++ front-page.php
@@ -11,8 +11,20 @@ get_header();?>
 			            <!-- <img class="head-pic" src=" <?php /*echo get_home_url(); */ ?> wp-content/uploads/2021/09/champagner-1071356_1920.jpg" /> -->
                         <img id="head-pic" src="<?php echo get_template_directory_uri(); ?>/img/headchampagne.jpg" />
 		            </div>
-                    <div>
-                        <?php /* get_category(); */ ?>
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+		                    <div id="category-pics">
+
+                    <?php
+                 
+                        echo do_shortcode('[product_categories]');
+                    ?>
+
+                </div>
+                    </div>
+                        </div>
+                            </div>
                     </div>
                 </div>
             </div>
