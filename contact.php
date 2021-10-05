8da3751ebbaae9501c425b0f90ffc5144e8e1581
Ivy Trainor <tytrainor@hotmail.com>
2021-10-05 15:10:27 +0200

    styled contact page a bit more

--- contact.php
+++ contact.php
@@ -6,19 +6,29 @@ get_header();?>
     <section>
         <div class="container">
             <div class="row">
-                <div class="col-md-12">
-                    <h1>Välkommen att kontakta oss</h1>
-                    <p> Fyll i kontaktformuläret till höger så återkommer vi inom 24h</p>
+                <div class="col-md-4 mt-5 mb-5 welcome-text">
+                    <h1>Välkommen att <br />kontakta oss</h1>
+                    <i class="infotext"> Fyll i kontaktformuläret till höger så återkommer vi inom 24h</i>
+
+
+
                 </div>
-            <div class="col-md-5">
+                <div class="col-md-5 border border-secondary mt-5 mb-5">
                     <div class="row">
-                        <div class="col-md-6">
-                            <h3 class="text-left">Kontakta oss</h3>
+                        <div class="col-md-7 mt-3 custom-contact-form">
+                            <!--<h3 class="text-left">Kontakta oss</h3>-->
+                            <?php echo do_shortcode('[contact-form-7 id="25" title="Contact form 1" ]'); ?>
                         </div>
+                        <p class="contact-phone">Vi gå även att nås på <i>tel: 08 123 45 678</i>
+                            mellan
+                            kl <b>09:00-16:00</b>
+                        </p>
                     </div>
-                    <div class="content-wrapper">
-                        <?php echo do_shortcode('[contact-form-7 id="25" title="Contact form 1" ]'); ?></div>
-                    <!-- <div class="col-xs-12 col-md-8 col-md-offset-2">
+                </div>
+
+                <!--<div class="content-wrapper">
+                    <?php echo do_shortcode('[contact-form-7 id="25" title="Contact form 1" ]'); ?></div>-->
+                <!-- <div class="col-xs-12 col-md-8 col-md-offset-2">
                         <h1>Kontakt</h1>
                         <form>
                             <label>Välj ett ärende:</label>
@@ -38,7 +48,7 @@ get_header();?>
                         </form>
                     <!-- < </div>
             <!-- add shot position here-->
-                    <!--</div>
+                <!--</div>
         </div>-->
     </section>
 </main>
