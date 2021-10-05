052b83fea90fc44a50a8d2cc4883cd7460f84967
Ivy Trainor <tytrainor@hotmail.com>
2021-10-05 10:23:33 +0200

    added policy page. Created template for it on WP. Added contact page. Created plugin in WP and added extension for it in contact.php

---
+++ contact.php
@@ -0,0 +1,47 @@
+<?php
+/*Template Name: Contact*/
+get_header();?>
+
+<main>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <!--<h1>Välkommen att kontakta oss</h1>
+                    <p> Fyll i kontaktformuläret till höger så återkommer vi inom 24h</p>
+                </div>
+                <div class="col-md-5">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h3 class="text-left">Kontakta oss</h3>
+                        </div>
+                    </div>-->
+                    <div>[contact-form-7 id="25" title="Contact form 1"]</div>
+                    <!-- <div class="col-xs-12 col-md-8 col-md-offset-2">
+                        <h1>Kontakt</h1>
+                        <form>
+                            <label>Välj ett ärende:</label>
+                            <select name="drop_down">
+                                <option>Kontakt</option>
+                                <option>Reklamation</option>
+                                <option>Faktura</option>
+                            </select>
+
+                            <label>Namn</label>
+                            <input type="text" />
+                            <label>E-post</label>
+                            <input type="email" />
+                            <label>Meddelande</label>
+                            <textarea class="textArea" cols="25" rows="10"></textarea>
+                            <input type="submit" value="Skicka" />
+                        </form>
+                    <!-- < </div>
+            <!-- add shot position here-->
+                    <!--</div>
+        </div>-->
+    </section>
+</main>
+
+<?php
+get_footer();
+?>
---
+++ privacy-policy.php
@@ -0,0 +1,89 @@
+<?php
+/*Template Name: Privacy policy*/
+get_header();?>
+
+<main>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h2 class="mini-headers">Integritetspolicy för Porslin Arkivet</h2>
+
+                    Vi värnar om din personliga integritet och eftersträvar alltid en hög nivå av dataskydd
+                    – i enlighet med GDPR.
+
+                    Datasekretess är mycket viktigt för WooCommerce Butiken och vi vill alltid vara transparenta med hur
+                    vi behandlar dina personuppgifter för att du ska känna dig trygg när du lämnar dina uppgifter till
+                    oss. Denna integritetspolicy förklarar hur vi samlar in och använder din personliga information. Den
+                    beskriver också dina rättigheter och hur du kan göra dem gällande.
+
+                    Du är alltid välkommen att kontakta oss vid eventuella frågor.
+                    <h2 class="mini-headers">PRIS POLICY</h2>
+                    Priserna som anges är alltid inklusive moms.
+
+                    Vi reserverar oss för eventuella tryck- och skrivfel. Om felaktigheter i pris och leveranstid eller
+                    andra omständigheter skulle infinna sig, så förbehåller sig Woocommerce Butiken rätten att ändra
+                    detta i efterhand. Vid dessa tillfällen så kontaktar vi dig och informerar om detta. Du har då din
+                    fulla rätt att antingen ändra eller annullera din beställning.
+                    Vi försöker alltid vara så tydliga som möjligt med hur vi behandlar dina personuppgifter.
+
+                    <h2 class="mini-headers">KORTBETALNING</h2>
+                    Vi accepterar VISA, Mastercard och American Express (exklusive Norge). Viktigt att veta är att vi
+                    erbjuder 3D Secure/SecureCode som är en säkrare lösning för dig som betalar med kort. Observera dock
+                    att det ej går att betala med Electron- eller Maestrokort då Svenska inlösare ej stöder
+                    online-handel med dessa.
+
+                    3D Secure är en teknik framtagen av VISA för att verifiera dig som konsument när du betalar med ditt
+                    kort på Internet. Standarden används även av Mastercard. Fördelar för dig som konsument är att du
+                    får en möjlighet att verifiera att du är den verkliga kortinnehavaren, vilket innebär mindre risk
+                    att kortet används i bedrägligt syfte.
+
+                    Våra kortbetalningar hanteras av Klarna payment services och de är licensierade för 3-D Secure
+                    (Verified by Visa och MasterCard SecureCode) och certifierade enligt PCI DSS. De följer bankernas
+                    krav på sekretess och säkerhetcertifieringar.
+
+                    <h2 class="mini-headers">ÅNGERRÄTT REKLAMATION</h2>
+                    Vi följer lagen om distansavtal enligt Konsumentverkets rekommendationer. Du har enligt
+                    distansavtalslagen rätt att returnera beställda felfria varor utan att ange någon anledning.
+                    Meddelande om detta ska lämnas så snart som möjligt, dock senast inom fjorton (14) dagar från det
+                    att varan mottagits.
+
+                    Om det kan påvisas fel på verket eller reproduktionen ersätter vi alltid ditt vara eller erbjuder
+                    ett motsvarande verk.
+
+
+                    <h2 class="mini-headers">LEVERANSVILLKOR</h2>
+                    Leverans sker normalt sett inom 7-14 dagar, men kan ta längre tid upp mot 20 dagar för fall då det
+                    beställda verket finns utställt eller liknande. Vid beställning av ett numrerat verk kan leverns
+                    komma att ske senare.
+
+                    Vi jobbar alltid för att kunna leverera din produkt så fort som möjligt.
+
+
+
+                    <h2 class="mini-headers">SUPPORT OCH KONTAKT</h2>
+                    Kontakta oss enkelt via vårt kontaktformulär. Vi försöker svara så fort vi kan, men var god och ge
+                    oss upp till 48 timmar att besvara då det kan variera beroende på aktuell belastning.
+
+
+
+                    Har du fortfarande frågor om hur vi behandlar dina uppgifter efter att ha läst denna policy?
+                    Välkommen att kontakta oss i din lokala WooCommerce Butik eller enligt nedan.
+
+
+                    <div>
+                        <p>Porslin Arkivet</p>
+                        <p>Banérgatan 1 115 22</p>
+                        <p>E-postadress: kundservice@porslinarkivet.se </p>
+                        <p>Telefon: 123 45 67</p>
+                        <p>Organisationsnummer:555888-0000</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+</main>
+
+<?php
+get_footer();?>
